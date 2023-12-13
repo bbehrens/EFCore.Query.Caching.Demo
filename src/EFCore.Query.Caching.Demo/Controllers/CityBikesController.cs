fbde4af66ff34cb986df0692a4e85caa8e7219ae
Brandon Behrens <brandonbehrens@gmail.com>
2023-12-13 13:22:14 -0600

    Upgrading project to .net 8 upgrading nugets.  Tweaking logging level to see output

--- src/EFCore.Query.Caching.Demo/Controllers/CityBikesController.cs
+++ src/EFCore.Query.Caching.Demo/Controllers/CityBikesController.cs
@@ -1,6 +1,7 @@
-﻿using EFCoreQueryCachingDemo.Controllers.Base;
-using EFCoreQueryCachingDemo.Models.Dto;
+﻿using Asp.Versioning;
+using EFCoreQueryCachingDemo.Controllers.Base;
 using EFCoreQueryCachingDemo.Models;
+using EFCoreQueryCachingDemo.Models.Dto;
 using EFCoreQueryCachingDemo.Services.Converters;
 using EFCoreQueryCachingDemo.Services.Repositories;
 using Microsoft.AspNetCore.Cors;
@@ -9,56 +10,56 @@ using System.Diagnostics;
 
 namespace EFCoreQueryCachingDemo.Controllers
 {
-	[ApiVersion("1.0")]
-	[ApiController]
-	[EnableCors("EnableCORS")]
-	[Produces("application/json")]
-	[Route("api/v{version:apiVersion}/[controller]")]
-	public class CityBikesController : BaseController<CityBikesController>
-	{
-		private readonly Stopwatch _stopwatch;
-		private readonly ICityBikesNetworksRepository _repository;
+    [ApiVersion("1.0")]
+    [ApiController]
+    [EnableCors("EnableCORS")]
+    [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class CityBikesController : BaseController<CityBikesController>
+    {
+        private readonly Stopwatch _stopwatch;
+        private readonly ICityBikesNetworksRepository _repository;
 
-		public CityBikesController(ICityBikesNetworksRepository repository)
-		{
-			_repository = repository;
-			_stopwatch = new Stopwatch();
-		}
+        public CityBikesController(ICityBikesNetworksRepository repository)
+        {
+            _repository = repository;
+            _stopwatch = new Stopwatch();
+        }
 
-		/// <summary>
-		/// Search CityBikes network by name
-		/// </summary>
-		/// <response code="200">Returns list of networks</response>
-		[HttpGet("searchCityBikesNetworkAsync")]
-		public async Task<ActionResult<IEnumerable<NetworkDto>>> SearchCityBikesNetworkAsync(string name)
-		{
-			_stopwatch.Start();
-			
-			IList<Network> networks = await _repository.SearchNetworksAsync(name).ConfigureAwait(false);
-			List<NetworkDto> result = networks.ToDtoModel();
-			
-			_stopwatch.Stop();
-			Logger.LogInformation("Searching for {name}, time elapsed (ms): {milliseconds}", name, _stopwatch.ElapsedMilliseconds);
+        /// <summary>
+        /// Search CityBikes network by name
+        /// </summary>
+        /// <response code="200">Returns list of networks</response>
+        [HttpGet("searchCityBikesNetworkAsync")]
+        public async Task<ActionResult<IEnumerable<NetworkDto>>> SearchCityBikesNetworkAsync(string name)
+        {
+            _stopwatch.Start();
 
-			return Ok(result);
-		}
+            IList<Network> networks = await _repository.SearchNetworksAsync(name).ConfigureAwait(false);
+            List<NetworkDto> result = networks.ToDtoModel();
 
-		/// <summary>
-		/// Get CityBikes networks
-		/// </summary>
-		/// <response code="200">Returns list of networks</response>
-		[HttpGet("getCityBikesNetworksAsync")]
-		public async Task<ActionResult<IEnumerable<NetworkDto>>> GetCityBikesNetworksAsync()
-		{
-			_stopwatch.Start();
+            _stopwatch.Stop();
+            Logger.LogInformation("Searching for {name}, time elapsed (ms): {milliseconds}", name, _stopwatch.ElapsedMilliseconds);
 
-			IList<Network> networks = await _repository.GetNetworksAsync().ConfigureAwait(false);
-			List<NetworkDto> result = networks.ToDtoModel();
+            return Ok(result);
+        }
 
-			_stopwatch.Stop();
-			Logger.LogInformation("Loading CityBikes networks, time elapsed (ms): {milliseconds}", _stopwatch.ElapsedMilliseconds);
+        /// <summary>
+        /// Get CityBikes networks
+        /// </summary>
+        /// <response code="200">Returns list of networks</response>
+        [HttpGet("getCityBikesNetworksAsync")]
+        public async Task<ActionResult<IEnumerable<NetworkDto>>> GetCityBikesNetworksAsync()
+        {
+            _stopwatch.Start();
 
-			return Ok(result);
-		}
-	}
+            IList<Network> networks = await _repository.GetNetworksAsync().ConfigureAwait(false);
+            List<NetworkDto> result = networks.ToDtoModel();
+
+            _stopwatch.Stop();
+            Logger.LogInformation("Loading CityBikes networks, time elapsed (ms): {milliseconds}", _stopwatch.ElapsedMilliseconds);
+
+            return Ok(result);
+        }
+    }
 }
